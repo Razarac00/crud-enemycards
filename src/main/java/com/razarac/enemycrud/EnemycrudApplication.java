7233bd97dfcbc3b617b1452d6eb82cc9cd34167f
John Orji <john.orji@cognizant.com>
2021-03-09 14:05:17 -0600

    setup skeleton for crud

---
+++ src/main/java/com/razarac/enemycrud/EnemycrudApplication.java
@@ -0,0 +1,13 @@
+package com.razarac.enemycrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnemycrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnemycrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/razarac/enemycrud/EnemycrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.razarac.enemycrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnemycrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
