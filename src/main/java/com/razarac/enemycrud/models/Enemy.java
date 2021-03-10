05f2cb71b77a930e5681d36dc1b48dbb63390ff6
John Orji <john.orji@cognizant.com>
2021-03-10 11:01:55 -0600

    removed entity annotations from service focused models

--- src/main/java/com/razarac/enemycrud/models/Enemy.java
+++ src/main/java/com/razarac/enemycrud/models/Enemy.java
@@ -2,32 +2,24 @@ package com.razarac.enemycrud.models;
 
 import java.util.List;
 
-import javax.persistence.*;
-
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity @Getter @Setter
+@Getter @Setter
 public class Enemy {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String name;
 
-    @ManyToMany @Column(nullable = false)
     private List<EnemyElement> weaknesses;
-    @ManyToMany @Column(nullable = false)
+    
     private List<EnemyElement> resistances;
-    @ManyToMany @Column(nullable = false)
+    
     private List<EnemyElement> immunities;
     
-    @Column(nullable = false)
     private String image;
     
-    @Column(nullable = false)
     private String description;
 
     public Enemy(Long id, String name, List<EnemyElement> weaknesses, List<EnemyElement> resistances, List<EnemyElement> immunities, String image, String description) {
--- src/main/java/com/razarac/enemycrud/models/EnemyElement.java
+++ src/main/java/com/razarac/enemycrud/models/EnemyElement.java
@@ -1,33 +1,18 @@
 package com.razarac.enemycrud.models;
 
-import java.util.List;
-
-import javax.persistence.*;
-
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity @Getter @Setter
+@Getter @Setter
 public class EnemyElement {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @Column(nullable = false)
     private String name;
-
-    @ManyToMany
-    private List<Enemy> enemies;
 
     public EnemyElement(Long id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public EnemyElement(Long id, String name, List<Enemy> enemies) {
-        this.id = id;
-        this.name = name;
-        this.enemies = enemies;
-    }
 }
