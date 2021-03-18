4b726cb83dd803bd80ff4a4647f4520261ff31a3
John Orji <john.orji@cognizant.com>
2021-03-18 11:25:48 -0500

    autowiring the repo, will need another service for elements

--- src/main/java/com/razarac/enemycrud/services/EnemyServiceImpl.java
+++ src/main/java/com/razarac/enemycrud/services/EnemyServiceImpl.java
@@ -1,8 +1,16 @@
 package com.razarac.enemycrud.services;
 
 import com.razarac.enemycrud.models.PageModel;
+import com.razarac.enemycrud.repository.EnemyCrudRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EnemyServiceImpl implements EnemyService {
+
+    @Autowired
+    private EnemyCrudRepository enemyCrudRepository;
 
     @Override
     public PageModel getEnemies(String search, Integer pageSize, Integer pageNumber) {
