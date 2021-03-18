9647cf9d7388da1e4ff981817467623e16971adb
John Orji <john.orji@cognizant.com>
2021-03-18 15:41:58 -0500

    added the building methods for elements

--- src/main/java/com/razarac/enemycrud/services/ElementServiceImpl.java
+++ src/main/java/com/razarac/enemycrud/services/ElementServiceImpl.java
@@ -1,8 +1,10 @@
 package com.razarac.enemycrud.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.razarac.enemycrud.models.EnemyElement;
+import com.razarac.enemycrud.entities.*;
+import com.razarac.enemycrud.models.*;
 
 import org.springframework.stereotype.Component;
 
@@ -19,6 +21,27 @@ public class ElementServiceImpl implements ElementService {
     public EnemyElement getElement(String name) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    private List<EEnemyElement> buildElements(List<String> elements) {
+        List<EEnemyElement> result = new ArrayList<EEnemyElement>(); 
+
+        for (String name : elements) {
+            EEnemyElement element = buildElement(name);
+            
+            result.add(element);
+        }
+
+        return result;
+    }
+
+    private EEnemyElement buildElement(String name) {
+        EEnemyElement element = EEnemyElement.builder().name(name).build(); 
+        element.setWeakEnemies(new ArrayList<EEnemy>());
+        element.setResistEnemies(new ArrayList<EEnemy>());
+        element.setImmuneEnemies(new ArrayList<EEnemy>());
+
+        return element;
     }
     
 }
