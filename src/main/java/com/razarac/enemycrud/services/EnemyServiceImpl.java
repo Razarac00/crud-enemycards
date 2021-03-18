842d1a43478661d5c9d7341de1be084c359bbf41
John Orji <john.orji@cognizant.com>
2021-03-18 17:16:25 -0500

    Setup getenemy functionality in the enemy service

--- src/main/java/com/razarac/enemycrud/services/EnemyServiceImpl.java
+++ src/main/java/com/razarac/enemycrud/services/EnemyServiceImpl.java
@@ -1,6 +1,12 @@
 package com.razarac.enemycrud.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.razarac.enemycrud.entities.EEnemy;
+import com.razarac.enemycrud.entities.EEnemyElement;
 import com.razarac.enemycrud.models.Enemy;
+import com.razarac.enemycrud.models.EnemyElement;
 import com.razarac.enemycrud.models.PageModel;
 import com.razarac.enemycrud.repository.EnemyCrudRepository;
 
@@ -21,8 +27,49 @@ public class EnemyServiceImpl implements EnemyService {
 
     @Override
     public Enemy getEnemy(String name) {
-        // TODO Auto-generated method stub
-        return null;
+        // Expecting a full name so there SHOULD only be one
+        List<EEnemy> enemies = enemyCrudRepository.findByNameContaining(name);
+
+        return convertEEnemy(enemies.get(0));
+    }
+
+    private List<Enemy> convertEEnemies(List<EEnemy> eEnemies) {
+        List<Enemy> result = new ArrayList<Enemy>();
+
+        for (EEnemy eEnemy : eEnemies) {
+            result.add(convertEEnemy(eEnemy));
+        }
+        return result;
+    }
+
+    private Enemy convertEEnemy(EEnemy eEnemy) {
+        List<EnemyElement> weaknesses = convertEElements(eEnemy.getWeaknesses());
+        List<EnemyElement> resistances = convertEElements(eEnemy.getResistances());
+        List<EnemyElement> immunities = convertEElements(eEnemy.getImmunities());
+
+        Enemy enemy = Enemy.builder()
+                        .name(eEnemy.getName())
+                        .description(eEnemy.getDescription())
+                        .image(eEnemy.getImage())
+                        .weaknesses(weaknesses)
+                        .resistances(resistances)
+                        .immunities(immunities)
+                        .build();
+        return enemy;
+    }
+
+    private List<EnemyElement> convertEElements(List<EEnemyElement> eElements) {
+        List<EnemyElement> result = new ArrayList<EnemyElement>();
+
+        for (EEnemyElement eElement : eElements) {
+            result.add(convertEElement(eElement));
+        }
+        return result;
+    }
+
+    private EnemyElement convertEElement(EEnemyElement eElement) {
+        EnemyElement element = EnemyElement.builder().name(eElement.getName()).id(eElement.getId()).build();
+        return element;
     }
     
 }
