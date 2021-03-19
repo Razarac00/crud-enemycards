4196b270cd9ba73e9f6dccc3146daf084ae0c236
John Orji <john.orji@cognizant.com>
2021-03-19 14:15:36 -0500

    spacing

--- src/main/java/com/razarac/enemycrud/services/EnemyService.java
+++ src/main/java/com/razarac/enemycrud/services/EnemyService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface EnemyService {
+    
     PageModel getEnemies(String search, Integer pageSize, Integer pageNumber);
+
     Enemy getEnemy(String name);
 }
