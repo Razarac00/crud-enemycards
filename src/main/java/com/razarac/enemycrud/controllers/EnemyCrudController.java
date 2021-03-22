bbeb57f2a084120c8b97662fb0fec805635dd139
John Orji <john.orji@cognizant.com>
2021-03-22 14:36:47 -0500

    fixed request mapping

--- src/main/java/com/razarac/enemycrud/controllers/EnemyCrudController.java
+++ src/main/java/com/razarac/enemycrud/controllers/EnemyCrudController.java
@@ -10,19 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/enemy")
 public class EnemyCrudController {
 
     @Autowired
     private EnemyService enemyService;
 
     @GetMapping
+    @RequestMapping("/enemies")
     public PageModel getEnemies(String searchName, Integer pageNumber, Integer pageSize) {
 
         return enemyService.getEnemies(searchName, pageSize, pageNumber);
     }
     
     @GetMapping
+    @RequestMapping("/enemy")
     public Enemy getEnemy(String searchName) {
 
         return enemyService.getEnemy(searchName);
