2b0c2f8e92b5351629e5e57832789579f24b46a8
John Orji <john.orji@cognizant.com>
2021-03-22 11:39:46 -0500

    added getenemies test

--- src/test/java/com/razarac/enemycrud/services/EnemyServiceImplTests.java
+++ src/test/java/com/razarac/enemycrud/services/EnemyServiceImplTests.java
@@ -1,13 +1,28 @@
 package com.razarac.enemycrud.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.razarac.enemycrud.entities.EEnemy;
+import com.razarac.enemycrud.models.Enemy;
+import com.razarac.enemycrud.models.PageModel;
 import com.razarac.enemycrud.repository.EnemyCrudRepository;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -37,4 +52,28 @@ public class EnemyServiceImplTests {
 
     @BeforeEach
     public void setup() {}
+
+    @Test
+    public void getEnemies_ReturnsEmptyPageModel_WhenCalled() {
+        // Arrange
+        String searchName = "";
+        Integer pageSize = 4;
+        Integer pageNumber = 0;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        List<EEnemy> eEnemyList = new ArrayList<EEnemy>();
+        Page<EEnemy> eEnemyPage = new PageImpl<EEnemy>(eEnemyList);
+        
+        Mockito.when(enemyCrudRepository.findByNameContaining(searchName, pageable)).thenReturn(eEnemyPage);
+        Mockito.when(enemyCrudRepository.findByNameContaining(searchName)).thenReturn(eEnemyList);
+        
+        // Act
+        var actual = enemyService.getEnemies(searchName, pageSize, pageNumber);
+        List<Enemy> expectedList = new ArrayList<Enemy>();
+
+        // Assert
+        assertTrue(actual instanceof PageModel);
+        assertEquals(expectedList, actual.getContent());
+        assertEquals(pageSize, actual.getPageSize());
+        assertEquals((pageSize * pageNumber), actual.getEnemyOffset());
+    } 
 }
