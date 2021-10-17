6dbaa77ec455d86d67be8019ca9bed82e7c19631
John Orji <orji.john@outlook.com>
2021-10-17 17:00:14 -0500

    Added some nice api ops and response documentation, also removed commented out code

--- src/main/java/com/razarac/enemycrud/config/SwaggerConfig.java
+++ src/main/java/com/razarac/enemycrud/config/SwaggerConfig.java
@@ -34,13 +34,4 @@ public class SwaggerConfig {
               .build();
   }
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("swagger-ui.html")
-//                .addResourceLocations("classpath:/META-INF/resources/");
-//
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
-
 }
